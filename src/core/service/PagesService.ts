7f0d91a338380de02b7f77875e1acd17e5c9ae12
platonfedorov <fedorov.platon.edu@gmail.com>
2021-10-29 12:54:39 +0300

    vb

--- src/core/service/PagesService.ts
+++ src/core/service/PagesService.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosResponse} from "axios";
 import {Status} from "../types/Status";
+import {backendAddress} from "../../../config.json";
 
 type Filenames = {
     filenames: string[]
@@ -7,7 +8,7 @@ type Filenames = {
 
 export class PagesService {
     public static async startDownload(filename: string): Promise<Status> {
-        const response = await axios.post('http://localhost:5000/pages/start', {
+        const response = await axios.post(`http://${backendAddress}:5000/pages/start`, {
             filename: filename
         });
 
@@ -15,25 +16,25 @@ export class PagesService {
     }
 
     public static async stopDownload(): Promise<Status> {
-        const response = await axios.post('http://localhost:5000/pages/stop');
+        const response = await axios.post(`http://${backendAddress}:5000/pages/stop`);
 
         return response.data;
     }
 
     public static async getStatus():Promise<Status> {
-        const response: AxiosResponse<Status> = await axios.get('http://localhost:5000/pages/status');
+        const response: AxiosResponse<Status> = await axios.get(`http://${backendAddress}:5000/pages/status`);
 
         return response.data;
     }
 
     public static async getFilenames():Promise<string[]> {
-        const response: AxiosResponse<Filenames> = await axios.get('http://localhost:5000/pages/filenames');
+        const response: AxiosResponse<Filenames> = await axios.get(`http://${backendAddress}:5000/pages/filenames`);
 
         return response.data.filenames;
     }
 
     public static async  splitFile(filename: string, machineCount: number, splitNames: string[]): Promise<string[]> {
-        const response: AxiosResponse<Filenames> = await axios.post('http://localhost:5000/pages/split', {
+        const response: AxiosResponse<Filenames> = await axios.post(`http://${backendAddress}:5000/pages/split`, {
             filename: filename,
             machineCount: machineCount,
             splitNames: splitNames
@@ -43,7 +44,7 @@ export class PagesService {
     }
 
     public static async deleteFiles(filenames: string[]): Promise<string[]> {
-        const response: AxiosResponse<Filenames> = await axios.post('http://localhost:5000//pages/delete/filenames', {
+        const response: AxiosResponse<Filenames> = await axios.post(`http://${backendAddress}:5000//pages/delete/filenames`, {
             filenames: filenames
         });
 
--- src/core/service/TitlesService.ts
+++ src/core/service/TitlesService.ts
@@ -1,21 +1,22 @@
 import axios, {AxiosResponse} from 'axios';
 import {Status} from '../types/Status';
+import {backendAddress} from "../../../config.json";
 
 export class TitlesService {
     public static async startDownload(): Promise<Status> {
-        const response: AxiosResponse<Status> = await axios.post('http://localhost:5000/titles/start');
+        const response: AxiosResponse<Status> = await axios.post(`http://${backendAddress}:5000/titles/start`);
 
         return response.data;
     }
 
     public static async stopDownload(): Promise<Status> {
-        const response: AxiosResponse<Status> = await axios.post('http://localhost:5000/titles/stop');
+        const response: AxiosResponse<Status> = await axios.post(`http://${backendAddress}/titles/stop`);
 
         return response.data;
     }
 
     public static async getStatus():Promise<Status> {
-        const response: AxiosResponse<Status> = await axios.get('http://localhost:5000/titles/status');
+        const response: AxiosResponse<Status> = await axios.get(`http://${backendAddress}:5000/titles/status`);
 
         return response.data;
     }
